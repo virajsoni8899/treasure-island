f1bda0db196540c698632ef7a40a9336b1d1ad29
Viraj soni <95369988+virajsoni8899@users.noreply.github.com>
2023-12-13 18:07:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+import random
+print("you are at the cross road were you want to go 'left' or 'right' ")
+direction = input()
+if direction == 'left':
+  print("you are at a mountain\n")
+  print("were do you want to go 'up' or 'down'")
+  direction = input()
+  if direction == 'up':
+    print("there is three cave which one you want to entre ")
+    print('cave1','cave2','cave3')
+    choose = input()
+    if choose == 'cave1':
+      print("game over you are killed by bear:")
+    elif choose == 'cave2':
+      print("you win! found treasure:")
+    elif choose == 'cave3':
+      print("game over you were killed by tigher")
+  else:
+    print("game over")
+elif direction == 'right':
+  print('game over')
+
